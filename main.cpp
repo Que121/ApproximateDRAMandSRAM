e687ee6ecd6e687be442b926582d1454981b893e
Que121 <1095076407@qq.com>
2022-09-02 21:53:43 +0800

    done

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ cv::Size dsize = Size(IMAGE_WIDTH, IMAGE_HEIGHT); // 图像大小
 
 u_int8_t BinaryTemp[8] = {0};
 
-cv::Mat src = cv::imread(TEST_PATH, IMREAD_COLOR); // jpeg格式
-// cv::Mat src = returnRawData(); // raw格式
+// cv::Mat src = cv::imread(TEST_PATH, IMREAD_COLOR); // jpeg格式
+cv::Mat src = returnRawData(); // raw格式
 cv::Mat dst = Mat ::zeros(dsize, CV_64FC1);
 // cv::Mat dst1 = Mat ::zeros(5, 5, CV_8UC1); //测试数组
 
